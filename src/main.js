0136aba0ec72b9a081d15f76d3097413c15e2263
Juanita Zuluaga Acosta <59172512+cantsbee@users.noreply.github.com>
2025-07-31 12:44:16 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -33,7 +33,7 @@ export async function redirectToAuthCodeFlow(clientId) {
   const params = new URLSearchParams();
   params.append("client_id", clientId);
   params.append("response_type", "code");
-  params.append("redirect_uri", "http://127.0.0.1:5173/callback");
+  params.append("redirect_uri", "https://api-git-main-juanitas-projects-4b540ac4.vercel.app/");
   params.append("scope", "user-read-private user-read-email user-top-read user-read-recently-played playlist-read-private user-library-read user-read-currently-playing user-read-playback-state");
   params.append("code_challenge_method", "S256");
   params.append("code_challenge", challenge);
@@ -69,7 +69,7 @@ export async function getAccessToken(clientId, code) {
   params.append("client_id", clientId);
   params.append("grant_type", "authorization_code");
   params.append("code", code);
-  params.append("redirect_uri", "http://127.0.0.1:5173/callback");
+  params.append("redirect_uri", "https://api-git-main-juanitas-projects-4b540ac4.vercel.app/");
   params.append("code_verifier", verifier);
 
   const result = await fetch("https://accounts.spotify.com/api/token", {
